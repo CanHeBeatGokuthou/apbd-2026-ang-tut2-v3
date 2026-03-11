ca8f235c42fddb84d3eae0cde3a75dd18fec5c39
Kajetan Borkowski <kajetan.borkowski.com@gmail.com>
2026-03-11 16:39:56 +0100

    Task4

--- APBD2.2/APBD2.2/Program.cs
+++ APBD2.2/APBD2.2/Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Change2");
 Console.WriteLine("Change3");
 Console.WriteLine("Change4");
+
+int CalculateAverage(int[] values)
+{
+    int avr = 0;
+    foreach (int n in  values)
+    {
+        avr += n;
+    }
+    return avr/values.Length;
+}
