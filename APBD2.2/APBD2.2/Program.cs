ce13bfa481c44c2e8046338e859177cb0df3207a
Kajetan Borkowski <kajetan.borkowski.com@gmail.com>
2026-03-11 15:20:37 +0100

    Task2 2commit

--- APBD2.2/APBD2.2/Program.cs
+++ APBD2.2/APBD2.2/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Change2");
+Console.WriteLine("Change3");
